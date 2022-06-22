0bad621b43c5f019eb1754da08b8348b2323a7a4
Evgeni Trofimov <55191884+default-INT@users.noreply.github.com>
2022-06-22 18:43:14 +0500

    [GAL-5] Init Node.js project

---
+++ config/app.ts
@@ -0,0 +1,6 @@
+import { corsOptions } from 'config/corsOptions';
+import express from 'express';
+
+export const app = express();
+
+app.use(corsOptions);
---
+++ index.ts
@@ -0,0 +1,12 @@
+import 'module-alias/register';
+import { app } from 'config/app';
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
