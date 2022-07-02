c57dac8ab49881ddb4b7b205f8e5642e8be8aad4
yauheni.trafimau <yauheni.trafimau@itechart-group.com>
2022-07-02 16:05:03 +0500

    [GAL-7] Connect MongoDB to Mongoose in project

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import 'module-alias/register';
 import { app } from 'config/app';
+import { connect } from 'mongoose';
+import { routes } from './config/routes';
 
 const port = 3000;
+
+connect(
+  'mongodb+srv://admin:18061806@gallery-cluster.9pixq.mongodb.net/?retryWrites=true&w=majority'
+).then(() => console.log('connected'));
+
+app.use(routes.gallery, (req, res) => res.sendStatus(500));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
